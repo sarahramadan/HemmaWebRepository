ed44b647597706f26433e2a181ea541305343ddd
sarhAlhamshary <engsaramagdy9288@gmail.com>
2018-09-22 14:58:14 +0200

    LOCAL
    LOCAL

--- js/controllers/general/LoginController.js
+++ js/controllers/general/LoginController.js
@@ -1,6 +1,5 @@
 ﻿angular.module('MetronicApp').controller('LoginController', function ($rootScope, $scope, $http, $timeout, CRUDFactory, $q, $filter, $state, $uibModal, $log, UserAccountFactory) {
     console.log("enter login controller");
-    $rootScope.ShowLoader = true;
     //redirect to login page 
     $scope.OpenLoginModel = function (IsRegister, IsEnterprise, IsGeneral) {
         $scope.MainObj = { PageType: IsRegister, PersonType: IsEnterprise, General: IsGeneral };
@@ -14,11 +13,19 @@
                 }
             }
         });
+
+        //modalInstance.result.then(function (selectedItem) {
+        //    $scope.selected = selectedItem;
+        //}, function () {
+        //    // $log.info('Modal dismissed at: ' + new Date());
+        //});
     }
     $scope.OpenLoginModel();
+    //if login is correct redirect to right page
 });
 
-angular.module('MetronicApp').controller('LoginModelController', function ($scope, $modalInstance, UserAccountFactory, $cookieStore, $rootScope, $state) {
+//var LoginModelController =
+angular.module('MetronicApp').controller('LoginModelController', function ($scope, $modalInstance, UserAccountFactory, MainObj, $cookieStore) {
 
     console.log("enter login model new");
     $scope.edit = false;
@@ -26,59 +33,44 @@ angular.module('MetronicApp').controller('LoginModelController', function ($scop
     $scope.newTeacherObj = {};
     $scope.newEnterpriseObj = {};
     $scope.ErrorMessage = [];
-    $rootScope.showLoader = false;
-    $scope.ForgetPassowrdFlag = false;
-    //$rootScope.submitted = true;
 
     $scope.saveData = function (formValid, type, obj) {
         $scope.edit = true;
         if (formValid && obj) {
-            $rootScope.showLoader = true;
             obj.IsEnterprise = type;
             UserAccountFactory.RegisterAccount(obj).then(function (data) {
                 console.log("data", data);
-                $rootScope.showLoader = false;
                 if (data.data.ErrorMessage && data.data.ErrorMessage.length > 0) {
-                    $scope.ShowMessage = type;
                     $scope.ErrorMessage = data.data.ErrorMessage;
                 } else {
-                    var page = data.data.Result.RedirectPage;
-                    var token = JSON.parse(data.data.Result.Token);
-                    $cookieStore.put('key', token.access_token);
-                    $scope.CloseModel();
-                    $state.go(page);
+                    //login
+                    $scope.login(formValid, data.data.Result.UserEmail, data.data.Result.UserPassword);
                 }
             });
-        } else {
-            angular.element('input.ng-invalid').first().focus();
         }
     };
-
-    $scope.login = function (formValid,obj) {
+    $scope.login = function (formValid, usermail, password) {
         $scope.edit = true;
         if (formValid) {
-            $rootScope.showLoader = true;
-            UserAccountFactory.SiginInAccount(obj).success(function (data, status, headers, config) {
-                console.log("dddd", data);
-                $rootScope.showLoader = false;
-                if (data.ErrorMessage && data.ErrorMessage.length > 0) {
-                    $scope.ErrorMessageLogin = data.ErrorMessage;
-                } else {
-                    var page = data.Result.RedirectPage;
-                    var token = JSON.parse(data.Result.Token);
-                    $cookieStore.put('key', token.access_token);
-                    $scope.CloseModel();
-                    $state.go(page);
+            UserAccountFactory.LoginAccount(usermail, password).success(function (data, status, headers, config) {
+                console.log("rettrun from login", data, status, headers, config);
+                $cookieStore.put('key', data.access_token);
+                document.getElementsByClassName('page-spinner-bar');
+                var element = angular.element(document.getElementsByClassName('page-spinner-bar')[0]);
+                element.removeClass('hide');
+                //window.location = "index.html";
+            })
+            .error(function (data, status, headers, config) {
+                var errorMsg = data.error;
+                if (data && data.error) {
+                    $scope.ErrorMessage =["كلمة المرور او البريد الالكتروني غير صحيحين من فضلك تاكد منهم"];
                 }
             });
             //reset form
-            //$scope.loginObj = {};         
-            //$scope.ResetModel();
-        } else {
-            angular.element('input.ng-invalid').first().focus();
+            $scope.loginObj = {};         
+            $scope.ResetModel();
         }
     };
-
     $scope.hasError = function (form, field, validation) {
         $scope.addForm = form;
         if (validation) {
@@ -86,70 +78,37 @@ angular.module('MetronicApp').controller('LoginModelController', function ($scop
         }
         return ($scope.addForm[field].$dirty && $scope.addForm[field].$invalid) || ($scope.edit && $scope.addForm[field].$invalid);
     };
-
+    $scope.HeaderTitel = function () {
+        var type = '';
+        if (!$scope.IsEnterprise) {
+            type = " معلم ";
+        } else {
+            type = " دار ";
+        }
+        if ($scope.RegisterNew) {
+            return  " تسجيل" + type + "جديد";
+        } else {
+            return  "تسجيل دخول مستخدم";
+        }
+    };
+    $scope.toggleRegister = function (IsEnterprise) {
+        $scope.IsEnterprise = IsEnterprise;
+        $scope.RegisterNew = !$scope.RegisterNew;
+        $scope.ResetModel();
+    };
     $scope.CloseModel = function () {
         $modalInstance.close();
     };
-
     $scope.ResetModel = function () {
         $scope.edit = false;
-        $rootScope.showLoader = false;
         $scope.addForm.$setPristine();
         $scope.addForm.$setUntouched();
-        $scope.ErrorMessage = [];
     };
 
-    $scope.GetAuthRedirectPage = function () {
-        UserAccountFactory.GetAuthUserRole().then(function (data) {
-            $scope.CloseModel();
-            if (data.data == RoleEnum.SystemAdmin) {
-                $state.go("ManageAccounts");
-            } else if (data.data == RoleEnum.AdminRole) {
-                $state.go("EnterpriseProfile");
-            }else if (data.data == RoleEnum.TeacherRole) {
-                $state.go("TeacherProfile");
-            }else {
-                $state.go("Home");
-            }
-        });
-    };
-
-    $scope.GetUnAuthRedirectPage = function (obj,data) {
-        UserAccountFactory.GetUnAuthUser(obj).then(function (data) {
-            debugger;
-            if (data.data!=null && data.data.ID && data.data.RoleID == RoleEnum.AdminRole && data.data.IsActive == false) {
-                $scope.CloseModel();
-                $state.go("AdminWelcome", { id: data.data.ID, UnAuthUserObj: data.data});
-            } else {
-                var errorMsg = data.error;
-                $scope.ErrorMessage = ["كلمة المرور او البريد الالكتروني غير صحيحين من فضلك تاكد منهم"];
-            }
-        });
-    }
-
-    $scope.ForgetPasswordToggle = function (form) {
-        $scope.addForm = form;
-        $scope.ForgetPassowrdFlag = !$scope.ForgetPassowrdFlag;
-        $scope.ResetModel();
-    };
-
-    $scope.ChangePassword = function (formValid, obj) {
-        $scope.edit = true;
-        if (formValid && obj) {
-            $rootScope.showLoader = true;
-            UserAccountFactory.ForgetPassword(obj).then(function (data) {
-                if (data.data.ErrorMessage && data.data.ErrorMessage.length > 0) {
-                    $scope.ShowMessage = type;
-                    $scope.ErrorMessage = data.data.ErrorMessage;
-                } else {
-                    //close model
-                    $scope.CloseModel();
-                }
-                $rootScope.showLoader = false;
-            });
-        } else {
-            angular.element('input.ng-invalid').first().focus();
-        }
+    if (MainObj) {
+        $scope.RegisterNew = MainObj.PageType;
+        $scope.IsEnterprise = MainObj.PersonType;
+        $scope.IsGeneral = MainObj.General
     }
 });
 
--- js/factories/UserAccountFactory.js
+++ js/factories/UserAccountFactory.js
@@ -1,6 +1,7 @@
 ﻿angular.module('MetronicApp').factory('UserAccountFactory', ['$http', '$rootScope', 'appConfigs', 'RequestFactory', function ($http, $rootScope, appConfigs, RequestFactory) {
     return {
         RegisterAccount: function (Obj) {
+            debugger;
             return RequestFactory.Request('POST', appConfigs.apiBaseURL + "UserAccount/RegisterAccount", Obj, null);
         },
         LoginAccount: function (username, password) {
@@ -11,34 +12,6 @@
               data: "userName=" + username + "&password=" + password + "&grant_type=password"
           });
             return result;
-        },
-        getCurrentUser: function () {
-            return RequestFactory.Request('GET', appConfigs.apiBaseURL + "UserAccount/CurrentUser", null, null);
-        },
-        SideMenu: function () {
-            return RequestFactory.Request('GET', appConfigs.apiBaseURL + "UserAccount/SideMenu", null, null);
-        },
-        GetAuthUserRole: function () {
-            return RequestFactory.Request('GET', appConfigs.apiBaseURL + "UserAccount/AuthUserRole", null, null);
-        },
-        GetUnAuthUser: function (obj) {
-            return RequestFactory.Request('POST', appConfigs.apiBaseURL + "UserAccount/UnAuthUser", obj, null);
-        },
-        ForgetPassword: function (obj) {
-            return RequestFactory.Request('POST', appConfigs.apiBaseURL + "Registers/ForgetPassword", obj, null);
-        },
-        ActivateAccount: function (UserID) {
-            return RequestFactory.Request('POST', appConfigs.apiBaseURL + "Registers/ActivateAccount/" + UserID,null, null);
-        },
-        GetUserAccountInfo: function (UID) {
-            return RequestFactory.Request('GET', appConfigs.apiBaseURL + "UserAccount/GetUserAccountInfo/" + UID, null, null);
-        },
-        UpdateAccount: function (Obj) {
-            return RequestFactory.Request('POST', appConfigs.apiBaseURL + "UserAccount/UpdateAccount/" + Obj, null, null);
-        },
-        SiginInAccount: function (obj) {
-            return RequestFactory.Request('POST', appConfigs.apiBaseURL + "UserAccount/SignIn", obj, null);
         }
-
     }
 }]);
